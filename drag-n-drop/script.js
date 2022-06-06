9369e89bf1c64c642167da86f5b3b49ef7abae0a
Maksim Chuchuk <cucukmaksim@gmail.com>
2022-06-06 13:50:19 +0300

    drag-n-drop

---
+++ drag-n-drop/script.js
@@ -0,0 +1,34 @@
+const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
+
+item.addEventListener('dragstart', (event) => {
+  event.target.classList.add('hold');
+  setTimeout(() => {
+    event.target.classList.add('hide');
+  }, 0);
+});
+
+item.addEventListener('dragend', (event) => {
+  event.target.className = 'item';
+});
+
+for (const placeholder of placeholders) {
+  placeholder.addEventListener('dragover', dragover);
+  placeholder.addEventListener('dragenter', dragenter);
+  placeholder.addEventListener('dragleave', dragleave);
+  placeholder.addEventListener('drop', dragdrop);
+}
+
+function dragover(event) {
+  event.preventDefault();
+}
+function dragenter(event) {
+  event.target.classList.add('hovered');
+}
+function dragleave(event) {
+  event.target.classList.remove('hovered');
+}
+function dragdrop(event) {
+  event.target.classList.remove('hovered');
+  event.target.append(item);
+}
